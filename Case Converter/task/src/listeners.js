06fc447d15cf26e7ea945dc755b4d6fced37288d
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-10-19 12:11:42 -0400

    Restructured listeners.js

--- Case Converter/task/src/listeners.js
+++ Case Converter/task/src/listeners.js
@@ -1,28 +1,22 @@
+const click = "click";
+
 const getText = () => document.querySelector("textarea").value;
 
 const setText = (newText) => document.querySelector("textarea").value = newText;
 
-const upperCaseButtonListener = () => {
-    const updatedText = getText().toUpperCase();
-    setText(updatedText);
-}
+document.querySelector("#upper-case").addEventListener(click,
+    () => setText(getText().toUpperCase()));
 
-const lowerCaseButtonListener = () => {
-    const updatedText = getText().toLowerCase();
-    setText(updatedText);
-}
+document.querySelector("#lower-case").addEventListener(click,
+    () => setText(getText().toLowerCase()));
 
 const firstLetterToUpperAndRestToLowerCase = () => (_, first, rest) => first.toUpperCase() + rest.toLowerCase();
 
-const properCaseButtonListener = () => {
-    let updatedText = getText().replace(/(\w)([\w']*\W*)/g, firstLetterToUpperAndRestToLowerCase());
-    setText(updatedText);
-}
+document.querySelector("#proper-case").addEventListener(click,
+    () => setText(getText().replace(/(\w)([\w']*\W*)/g, firstLetterToUpperAndRestToLowerCase())));
 
-const sentencesCaseButtonListener = () => {
-    let updatedText = getText().replace(/(\w)([^.]*\.?)/g, firstLetterToUpperAndRestToLowerCase());
-    setText(updatedText);
-}
+document.querySelector("#sentence-case").addEventListener(click,
+    () => setText(getText().replace(/(\w)([^.]*\.?)/g, firstLetterToUpperAndRestToLowerCase())));
 
 const download = (fileName, text) => {
     let element = document.createElement('a');
@@ -37,12 +31,5 @@ const download = (fileName, text) => {
     document.body.removeChild(element);
 }
 
-const saveTextFileListener = () => {
-    download("text.txt", getText())
-}
-
-document.querySelector("#upper-case").addEventListener("click", upperCaseButtonListener);
-document.querySelector("#lower-case").addEventListener("click", lowerCaseButtonListener);
-document.querySelector("#proper-case").addEventListener("click", properCaseButtonListener);
-document.querySelector("#sentence-case").addEventListener("click", sentencesCaseButtonListener);
-document.querySelector("#save-text-file").addEventListener("click", saveTextFileListener);
+document.querySelector("#save-text-file").addEventListener(click,
+    () => download("text.txt", getText()));
